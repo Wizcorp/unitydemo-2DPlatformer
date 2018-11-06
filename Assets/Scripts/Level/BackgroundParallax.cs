8195cce3f70206352eb7e821416fff6e909d6725
Toni Georgiev <georgiev.toni@gmail.com>
2018-11-07 02:11:51 +0900

    Fixed the background parallax
    The old parallax was moving the backgrounds in the wrong direction and it was somewhat more confusing.
    The backgrounds should not move opposite the camera movement, but along side it with fraction of its displacement.
    That way those backgrounds that are at a greater distance will move closely with the camera, appearing to stay still in the distance, while those closer (smaller fraction) will appear to move faster. And the closest one (foreground) is naturally the fastest because it doesn't follow the camera at all.

--- Assets/Scripts/Level/BackgroundParallax.cs
+++ Assets/Scripts/Level/BackgroundParallax.cs
@@ -3,11 +3,8 @@ using System.Collections;
 
 public class BackgroundParallax : MonoBehaviour
 {
-	public Transform[] backgrounds;				// Array of all the backgrounds to be parallaxed.
-	public float parallaxScale;					// The proportion of the camera's movement to move the backgrounds by.
-	public float parallaxReductionFactor;		// How much less each successive layer should parallax.
-	public float smoothing;						// How smooth the parallax effect should be.
-
+	public Transform[]  backgrounds;				// Array of all the backgrounds to be parallaxed.
+    public Vector2      parallaxFactor;
 
 	private Transform cam;						// Shorter reference to the main camera's transform.
 	private Vector3 previousCamPos;				// The postion of the camera in the previous frame.
@@ -19,30 +16,23 @@ public class BackgroundParallax : MonoBehaviour
 		cam = Camera.main.transform;
 	}
 
-
 	void Start ()
 	{
 		// The 'previous frame' had the current frame's camera position.
 		previousCamPos = cam.position;
 	}
 
-
 	void Update ()
 	{
-		// The parallax is the opposite of the camera movement since the previous frame multiplied by the scale.
-		float parallax = (previousCamPos.x - cam.position.x) * parallaxScale;
+        Vector2 cameraDisplacement = cam.position - previousCamPos;
 
 		// For each successive background...
 		for(int i = 0; i < backgrounds.Length; i++)
 		{
-			// ... set a target x position which is their current position plus the parallax multiplied by the reduction.
-			float backgroundTargetPosX = backgrounds[i].position.x + parallax * (i * parallaxReductionFactor + 1);
+            int distance = backgrounds.Length - i; // layers are aranged back to front
 
-			// Create a target position which is the background's current position but with it's target x position.
-			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
-
-			// Lerp the background's position between itself and it's target position.
-			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+            Vector2 displacement = cameraDisplacement * parallaxFactor * distance;
+            backgrounds[i].position += new Vector3(displacement.x, displacement.y, 0f);
 		}
 
 		// Set the previousCamPos to the camera's position at the end of this frame.
